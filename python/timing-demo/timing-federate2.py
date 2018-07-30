d0cef532ea11beb6cfa8246693d85169104dc263
Ryan Mast <mast9@llnl.gov>
2018-07-30 16:47:36 -0700

    Move changes from HELICS-src repository to examples

--- python/timing-demo/timing-federate2.py
+++ python/timing-demo/timing-federate2.py
@@ -1,5 +1,4 @@
 import helics as h
-import random
 
 def get_input(grantedtime):
 
@@ -65,8 +64,9 @@ def create_value_federate(deltat=1.0, fedinitstring="--federates=1"):
 
 def destroy_value_federate(fed):
     status = h.helicsFederateFinalize(fed)
-
+    assert status == 0
     status, state = h.helicsFederateGetState(fed)
+    assert status == 0
     assert state == 3
 
     h.helicsFederateFree(fed)
@@ -125,4 +125,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
