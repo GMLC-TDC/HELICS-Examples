45feec1c4d39030ddd56270878d0bdafc2af43c4
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-03-11 14:23:44 -0600

    Update timing examples

--- python/timing-demo/timing-federate2.py
+++ python/timing-demo/timing-federate2.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
 import helics as h
+
 
 def get_input(grantedtime):
 
     valid_input = False
     while not valid_input:
-        print("Enter request_time (int) (and value_to_send (float)) [e.g.: 4, 10.0]: ", end="")
+        print(
+            "Enter request_time (int) (and value_to_send (float)) [e.g.: 4, 10.0]: ",
+            end="",
+        )
         string = input()
         string = string.strip()
         request_time_str = string.replace(",", " ").split(" ")[0]
@@ -13,14 +18,18 @@ def get_input(grantedtime):
             if request_time <= grantedtime:
                 raise RuntimeError("Cannot proceed here because invalid input.")
         except:
-            print("request_time has to be an 'int' and has to be greater than grantedtime.")
+            print(
+                "request_time has to be an 'int' and has to be greater than grantedtime."
+            )
             valid_input = False
             continue
         else:
             valid_input = True
 
         try:
-            value_to_send = string.replace(request_time_str, "").strip().strip(",").strip()
+            value_to_send = (
+                string.replace(request_time_str, "").strip().strip(",").strip()
+            )
         except:
             value_to_send = None
             valid_input = True
@@ -38,54 +47,49 @@ def get_input(grantedtime):
     return request_time, value_to_send
 
 
+def create_value_federate(deltat=1.0, fedinitstring="--federates=1 --tick=0"):
 
-def create_value_federate(deltat=1.0, fedinitstring="--federates=1"):
+    fedinfo = h.helicsCreateFederateInfo()
 
-    fedinfo = h.helicsFederateInfoCreate()
+    h.helicsFederateInfoSetCoreName(fedinfo, "TestB Federate")
 
-    status = h.helicsFederateInfoSetFederateName(fedinfo, "TestB Federate")
-    assert status == 0
+    h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "zmq")
 
-    status = h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "zmq")
-    assert status == 0
+    h.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring)
 
-    status = h.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring)
-    assert status == 0
+    h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_delta, deltat)
 
-    status = h.helicsFederateInfoSetTimeDelta(fedinfo, deltat)
-    assert status == 0
+    h.helicsFederateInfoSetIntegerProperty(fedinfo, h.helics_property_int_log_level, 1)
 
-    status = h.helicsFederateInfoSetLoggingLevel(fedinfo, 1)
-    assert status == 0
-
-    fed = h.helicsCreateCombinationFederate(fedinfo)
+    fed = h.helicsCreateCombinationFederate("TestB Federate", fedinfo)
 
     return fed
 
+
 def destroy_value_federate(fed):
-    status = h.helicsFederateFinalize(fed)
-    assert status == 0
-    status, state = h.helicsFederateGetState(fed)
-    assert status == 0
-    assert state == 3
+    h.helicsFederateFinalize(fed)
+
+    state = h.helicsFederateGetState(fed)
 
     h.helicsFederateFree(fed)
 
     h.helicsCloseLibrary()
 
+
 def main():
 
     fed = create_value_federate()
 
-    pubid = h.helicsFederateRegisterGlobalTypePublication(fed, "federate2-to-federate1", h.HELICS_DATA_TYPE_STRING, "")
-    subid = h.helicsFederateRegisterSubscription (fed, "federate1-to-federate2", "double", "")
+    pubid = h.helicsFederateRegisterGlobalTypePublication(
+        fed, "federate2-to-federate1", "string", ""
+    )
+    subid = h.helicsFederateRegisterSubscription(fed, "federate1-to-federate2", "")
     epid = h.helicsFederateRegisterGlobalEndpoint(fed, "endpoint2", "")
 
-    # print("Setting default value")
-    h.helicsSubscriptionSetDefaultDouble(subid, 0)
+    h.helicsInputSetDefaultNamedPoint(subid, "", 0)
 
     print("Entering execution mode")
-    h.helicsFederateEnterExecutionMode(fed)
+    h.helicsFederateEnterExecutingMode(fed)
 
     grantedtime = -1
     while True:
@@ -96,29 +100,32 @@ def main():
             break
         while grantedtime < stop_at_time:
             print(">>>>>>>> Requesting time = {}".format(stop_at_time))
-            status, grantedtime = h.helicsFederateRequestTime(fed, stop_at_time)
-            assert status == 0
+            grantedtime = h.helicsFederateRequestTime(fed, stop_at_time)
+            grantedtime = int(grantedtime)
+
             if grantedtime != stop_at_time:
-                status, value = h.helicsSubscriptionGetString(subid)
-                assert status == 0
+                value = h.helicsSubscriptionGetKey(subid)
                 print("Interrupt value '{}' from Federate 1".format(value))
             print("<<<<<<<< Granted Time = {}".format(grantedtime))
-        assert grantedtime == stop_at_time, "stop_at_time = {}, grantedtime = {}".format(stop_at_time, grantedtime)
-        if value_to_send is not None and value_to_send != '':
+        assert (
+            grantedtime == stop_at_time
+        ), "stop_at_time = {}, grantedtime = {}".format(stop_at_time, grantedtime)
+        if value_to_send is not None and value_to_send != "":
             print("Sending '{}' to Federate 1".format(value_to_send))
-            status = h.helicsPublicationPublishString(pubid, str(value_to_send))
-            assert status == 0
-            status = h.helicsEndpointSendMessageRaw(epid, "endpoint1", str(value_to_send))
-            assert status == 0
+            h.helicsPublicationPublishString(pubid, str(value_to_send))
 
-        status, value = h.helicsSubscriptionGetString(subid)
-        assert status == 0
+            h.helicsEndpointSendMessageRaw(epid, "endpoint1", str(value_to_send))
+
+        value = h.helicsSubscriptionGetKey(subid)
         print("Received value '{}' from Federate 1".format(value))
         while h.helicsEndpointHasMessage(epid):
             value = h.helicsEndpointGetMessage(epid)
-            print("Received message '{}' at time {} from Federate 1".format(value.data, value.time))
+            print(
+                "Received message '{}' at time {} from Federate 1".format(
+                    value.data, value.time
+                )
+            )
         print("----------------------------------")
-
 
     destroy_value_federate(fed)
 
