d402b2c59836cda6b2d089c354a18e18f60a37e7
Ryan Mast <nightlark@users.noreply.github.com>
2019-06-01 12:52:49 -0700

    Test upstream HELICS PRs and report results to the PR issue

---
+++ .ci/report-results.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+case ${AGENT_JOBSTATUS} in
+    *Succeeded*)
+        BUILD_MESSAGE=":tada: HELICS-Examples integration test passed (${BUILD_BUILDURI})"
+    ;;
+    *Failed*)
+        BUILD_MESSAGE=":confused: HELICS-Examples integration test had issues (${BUILD_BUILDURI})"
+    ;;
+esac
+
+# Report build status to PR
+if [[ "${BUILD_MESSAGE}" != "" ]]; then
+    echo "Reporting build status $AGENT_JOBSTATUS to github.com/${HELICS_PR_SLUG}/issues/${HELICS_PR_NUM}"
+    body='{"body": "'${BUILD_MESSAGE}'"}'
+    curl -s -X POST \
+        -H "User-Agent: HELICS-bot" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/json" \
+        -H "Authorization: token ${HELICSBOT_GH_TOKEN}" \
+        -d "$body" \
+        https://api.github.com/repos/${HELICS_PR_SLUG}/issues/${HELICS_PR_NUM}/comments
+fi
