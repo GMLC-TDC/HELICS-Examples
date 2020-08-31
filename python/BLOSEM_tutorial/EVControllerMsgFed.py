5289762cade3a79bac482dca7ef246a38cc1ad01
Trevor Hardy <trevor.hardy@pnnl.gov>
2020-08-31 12:36:23 -0700

    Update tutorial example with more robust timing
    Switched the Controller over to timing that allows it to respond to messages whenever it receives them.
    
    Switched the EV federate over to a 1 minute periodic timing (instead of timedelta) to allow it to get it ready to simulate the physics of the battery using value signals. I also restructured the code just a bit so that the physics simulation that is currently being done is clearly delineated with the hopes that this will make it easier to slot in the battery physics model Allison will be working on.

--- python/BLOSEM_tutorial/EVControllerMsgFed.py
+++ python/BLOSEM_tutorial/EVControllerMsgFed.py
@@ -1,6 +1,12 @@
 """
 Created on 8/27/2020
 
+This is a simple EV charge controller federate that manages the charging at
+a set of charging terminals in a hypothetical EV garage. It receives periodic
+SOC messages from each EV (associated with a particular charging terminal)
+and sends back a message indicating whether the EV should continue charging
+or not (based on whether it is full).
+
 @author: Allison M. Campbell
 allison.m.campbell@pnnl.gov
 """
@@ -17,10 +23,19 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
-#
-
 
 def destroy_federate(fed):
+    '''
+    As part of ending a HELICS co-simulation it is good housekeeping to
+    formally destroy a federate. Doing so informs the rest of the
+    federation that it is no longer a part of the co-simulation and they
+    should proceed without it (if applicable). Generally this is done
+    when the co-simulation is complete and all federates end execution
+    at more or less the same wall-clock time.
+
+    :param fed: Federate to be destroyed
+    :return: (none)
+    '''
     status = h.helicsFederateFinalize(fed)
     h.helicsFederateFree(fed)
     h.helicsCloseLibrary()
@@ -28,12 +43,10 @@ def destroy_federate(fed):
 
 
 if __name__ == "__main__":
-
-
-    #################################  Registering  federate from json  ########################################
+    ##############  Registering  federate from json  ##########################
     fed = h.helicsCreateMessageFederateFromConfig("EVControllerconfig.json")
     federate_name = h.helicsFederateGetName(fed)
-    print(federate_name)
+    logging.info(f'Created federate {federate_name}')
 
 
     #### Register endpoint #####
@@ -42,63 +55,89 @@ if __name__ == "__main__":
     end_name = h.helicsEndpointGetName(endid)
     logger.info("Registered Endpoint ---> {}".format(end_name))
 
-
-
-    ######################   Entering Execution Mode  ##########################################################
-
+    ##############  Entering Execution Mode  ##################################
     h.helicsFederateEnterExecutingMode(fed)
-
+    logger.info('Entered HELICS execution mode')
 
     hours = 24*7 # one week
     total_interval = int(60 * 60 * hours)
-    update_interval = 30*60 # updates every 30 minutes
     grantedtime = -1
-#
 
-    instructions = []
-    # the EV sent its first message at 15min
-    # start the controller at 10min
-    starttime = 10*60
+    # It is common in HELICS for controllers to have slightly weird timing
+    #   Generally, controllers only need to produce new control values when
+    #   their inputs change. Because of this, it is common to have them
+    #   request a time very far in the future (helics_time_maxtime) and
+    #   when a signal arrives, they will be granted a time earlier than
+    #   that, recalculate the control output and request a very late time
+    #   again.
+
+    # There appears to be a bug related to maxtime in HELICS 2.4 that can
+    #   can be avoided by using a slightly smaller version of maxtime
+    #   (helics_time_maxtime is the largest time that HELICS can internally
+    #   represent and is an approximation for a point in time very far in
+    #   in the future).
+    fake_max_time = h.helics_time_maxtime/1000
+    starttime = fake_max_time
+    logger.debug(f'Requesting initial time {starttime}')
     grantedtime = h.helicsFederateRequestTime (fed, starttime)
-
-
+    logger.debug(f'Granted time {grantedtime}')
     t = grantedtime
+
+
     time_sim = []
     soc = {}
 
     while t < total_interval:
-        new_message = False
+
+        # In HELICS, when multiple messages arrive at an endpoint they
+        # queue up and are popped off one-by-one with the
+        #   "helicsEndpointHasMessage" API call. When that API doesn't
+        #   return a message, you've processed them all.
         while h.helicsEndpointHasMessage(endid):
-            new_message = True
-            # 1. Receive SOC
+
+            # Get the SOC from the EV/charging terminal in question
             msg = h.helicsEndpointGetMessageObject(endid)
             currentsoc = h.helicsMessageGetString(msg)
             source = h.helicsMessageGetOriginalSource(msg)
-            # 2. Send instructions
-            print(t/3600,currentsoc)
-            if float(currentsoc) <= 0.9:
+            logger.debug(f'Received message from endpoint {source}'
+                         f' at time {t}'
+                         f' with SOC {currentsoc}')
+
+            # Send back charging command based on current SOC
+            #   Our very basic protocol:
+            #       If the SOC is less than soc_full keep charging (send "1")
+            #       Otherwise, stop charging (send "0")
+            soc_full = 0.9
+            if float(currentsoc) <= soc_full:
                 instructions = 1
             else:
                 instructions = 0
             message = str(instructions)
             h.helicsEndpointSendMessageRaw(endid, source, message)
+            logger.debug(f'Sent message to endpoint {source}'
+                         f' at time {t}'
+                         f' with payload {instructions}')
 
-            # Store SOC for later use
+            # Store SOC for later analysis/graphing
             if source not in soc:
                 soc[source] = []
-                print(f'Keys in dictionary: {soc.keys()}')
             soc[source].append(float(currentsoc))
-        if not new_message:
-            print('NO MESSAGE RECEIVED AT TIME ',t/3600)
 
         time_sim.append(t)
 
-        grantedtime = h.helicsFederateRequestTime (fed, (t+update_interval))
-
+        # Since we've dealt with all the messages that are queued, there's
+        #   nothing else for the federate to do until/unless another
+        #   message comes in. Request a time very far into the future
+        #   and take a break until/unless a new message arrives.
+        logger.debug(f'Requesting time {fake_max_time}')
+        grantedtime = h.helicsFederateRequestTime (fed, fake_max_time)
+        logger.info(f'Granted time: {grantedtime}')
         t = grantedtime
 
-    #print(np.array(time_sim))
-    #print(np.array(time_sim)/60.0)
+    # Close out co-simulation execution cleanly now that we're done.
+    destroy_federate(fed)
+
+    # Printing out final results graphs for comparison/diagnostic purposes.
     xaxis = np.array(time_sim)/3600
     y = []
     for key in soc:
@@ -135,4 +174,4 @@ if __name__ == "__main__":
     plt.show()
 
 
-    destroy_federate(fed)
+
