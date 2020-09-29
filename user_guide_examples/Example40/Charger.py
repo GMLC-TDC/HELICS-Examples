3048c4161a2361fc39a270ef80e6dcb442c49774
Trevor Hardy <trevor.hardy@pnnl.gov>
2020-09-29 13:32:16 -0700

    Add documentation for calc_charging_voltage function

--- user_guide_examples/Example40/Charger.py
+++ user_guide_examples/Example40/Charger.py
@@ -41,6 +41,16 @@ def destroy_federate(fed):
 
 
 def calc_charging_voltage(EV_list):
+    '''
+    This function uses the pre-defined charging powers and maps them to
+    standard (more or less) charging voltages. This allows the charger
+    to apply an appropriately modeled voltage to the EV based on the
+    charging power level
+
+    :param EV_list: Value of "1", "2", or "3" to indicate charging level
+    :return: charging_voltage: List of charging voltages corresponding
+            to the charging power.
+    '''
     charging_voltage = []
     # Ignoring the difference between AC and DC voltages for this application
     charge_voltages = [120, 240, 630]
--- user_guide_examples/Example46/Charger.py
+++ user_guide_examples/Example46/Charger.py
@@ -76,6 +76,17 @@ def get_new_EV(numEVs):
 
 
 def calc_charging_voltage(EV_list):
+    '''
+    This function uses the pre-defined charging powers and maps them to
+    standard (more or less) charging voltages. This allows the charger
+    to apply an appropriately modeled voltage to the EV based on the
+    charging power level
+
+    :param EV_list: Value of "1", "2", or "3" to indicate charging level
+    :return: charging_voltage: List of charging voltages corresponding
+            to the charging power.
+    '''
+
     charging_voltage = []
     # Ignoring the difference between AC and DC voltages for this application
     charge_voltages = [120, 240, 630]
