a4c34e7af938b7f744c9393b98b48c6a5546a2c7
Trevor Hardy <trevor.hardy@pnnl.gov>
2021-06-16 16:35:20 -0700

    Updating advanced default example with some (but not) necessary changes or v3
    - Add extra time request at end of federate execution to clear any pending messages.
    - helicsSubscriptionGetKey() - > helicsSubscriptionGetTarget()
    - helicsPublicationGetKey() -> helicsPublicationGetName()
    - Update logging level names to new v3 standard
    - Update parameters for helicsEndpointSendBytesTo()
    - Use HELICS_TIME_MAXTIME instead of fake_max_time

--- user_guide_examples/advanced/advanced_default/Charger.py
+++ user_guide_examples/advanced/advanced_default/Charger.py
@@ -39,6 +39,10 @@ def destroy_federate(fed):
     :param fed: Federate to be destroyed
     :return: (none)
     '''
+    
+    # Adding extra time request to clear out any pending messages to avoid
+    #   annoying errors in the broker log. Any message are tacitly disregarded.
+    grantedtime = h.helicsFederateRequestTime(fed, h.HELICS_TIME_MAXTIME)
     status = h.helicsFederateFinalize(fed)
     h.helicsFederateFree(fed)
     h.helicsCloseLibrary()
@@ -159,13 +163,13 @@ if __name__ == "__main__":
     subid = {}
     for i in range(0, sub_count):
         subid[i] = h.helicsFederateGetInputByIndex(fed, i)
-        sub_name = h.helicsSubscriptionGetKey(subid[i])
+        sub_name = h.helicsSubscriptionGetTarget(subid[i])
         logger.debug(f'\tRegistered subscription---> {sub_name}')
 
     pubid = {}
     for i in range(0, pub_count):
         pubid[i] = h.helicsFederateGetPublicationByIndex(fed, i)
-        pub_name = h.helicsPublicationGetKey(pubid[i])
+        pub_name = h.helicsPublicationGetName(pubid[i])
         logger.debug(f'\tRegistered publication---> {pub_name}')
 
 
@@ -228,7 +232,7 @@ if __name__ == "__main__":
             #   uses the latest value provided by the battery model.
             charging_current = h.helicsInputGetDouble((subid[j]))
             logger.debug(f'\tCharging current: {charging_current:.2f} from '
-                         f'input {h.helicsSubscriptionGetKey(subid[j])}')
+                         f'input {h.helicsSubscriptionGetTarget(subid[j])}')
 
             # New EV is in place after removing charge from old EV,
             # as indicated by the zero current draw.
@@ -289,7 +293,7 @@ if __name__ == "__main__":
                 logger.debug(f'Sent message from endpoint {endpoint_name}'
                              f' to destination {destination_name}'
                              f' at time {grantedtime}'
-                             f' with payload SOC {currentsoc[j]:4f}')
+                             f' with payload SOC {message}')
 
         # Calculate the total power required by all chargers. This is the
         #   primary metric of interest, to understand the power profile
