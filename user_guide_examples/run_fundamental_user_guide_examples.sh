d4b43d17931669d4cbad2a994046c10bbc8626d0
Trevor Hardy <trevor.hardy@pnnl.gov>
2022-06-28 12:11:19 -0700

    Add shell scripts to run all fundamental and advanced User Guide example

---
+++ user_guide_examples/run_fundamental_user_guide_examples.sh
@@ -0,0 +1,36 @@
+#! /bin/zsh
+
+# Setting environment variable to prevent plots from printing
+# A faux backend that displays nothing
+export MPLBACKEND=AGG
+
+# Stop running examples once one of them fails
+set -o errexit
+
+# Delete out the existing log files so we don't fool ourselves with old results
+find . -name "*.log" | xargs rm
+
+# Fundamental
+echo "\n###### Fundamental default #####"
+cd ./fundamental/fundamental_default
+helics run --path=./fundamental_default_runner.json 
+
+echo "\n###### Fundamental integration ######"
+cd ../fundamental_integration
+helics run --path=./fundamental_integration_runner.json
+
+echo "\n###### Fundamental combination ######"
+cd ../fundamental_message_comm/combo
+helics run --path=./fundamental_combo_runner.json
+
+echo "\n##### Fundamental endpoints ######"
+cd ../endpoints
+helics run --path=./fundamental_endpoints_runner.json
+
+echo "\n###### Fundamental native filters ######"
+cd ../filter_native
+helics run --path=./fundamental_filter_native_runner.json
+
+echo "\n##### Fundamental filter federate #####"
+cd ../filter_federate
+helics run --path=./fundamental_filter_runner.json
