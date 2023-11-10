160409d079d5a95bc08d37e7eef76d4748f8e9a8
trevorhardy <trevor.hardy@pnnl.gov>
2023-11-10 13:14:36 -0800

    Update GridLAB-D example file structure and add output files
    Simplifies file structure so that common input files and executables can be used more easily. Also add outputs so users can more easily validate their results.

--- user_guide_examples/misc/gridlabd_example_1/Example_1a/run_example_1a.sh
+++
@@ -1,7 +0,0 @@
-(helics_broker -t="zmq" --federates=2 --name=mainbroker &> ./broker.log &)
-cd Transmission
-(exec python Transmission_simulator.py &> ./TransmissionSim.log &)
-cd ..
-cd Distribution
-(gridlabd IEEE_123_feeder_0.glm &> ./DistributionSim.log &)
-cd ..
