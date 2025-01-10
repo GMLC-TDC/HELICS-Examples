1f47bc85a7263b7de5633f7cc7161b3b06b43f6b
trevorhardy <trevor.hardy@pnnl.gov>
2025-01-09 16:03:21 -0800

    Update shell script to run testing on advnaced user guide

--- user_guide_examples/run_advanced_user_guide_examples.sh
+++ user_guide_examples/run_advanced_user_guide_examples.sh
@@ -31,26 +31,56 @@ helics run --path=./federation_2_runner.json &
 cd ../federation_3
 helics run --path=./federation_3_runner.json
 
-echo "\n##### Advanced input and output ######"
+# Example does not run due to filter bug
+# HELICS issue #2524
+# echo "\n##### Advanced input and output ######"
 cd ../../../advanced_input_output
-helics run --path=./fib_runner.json
+# helics run --path=./fib_runner.json
 
-echo "\n###### Advanced iteration ######"
+echo "\n###### Iteration ######"
 cd ../advanced_iteration
 helics run --path=./advanced_iteration_runner.json
 
-echo "\n##### Advanced multi-input #####"
+echo "\n##### Multi-input #####"
 cd ../advanced_message_comm/multi_input
 helics run --path=./multi_input_runner.json
 
-echo "\n###### Advanced query ######"
+echo "\n###### Queries ######"
 cd ../query
 helics run --path=./query_runner.json
 
-echo "\n##### Advanced translators ######"
+echo "\n##### Translators ######"
 cd ../translators
 helics run --path=./translator_runner.json
 
 echo "\n##### Advanced default ######"
 cd ../../advanced_default
 helics run --path=./advanced_default_runner.json
+
+echo "\n##### Async time request ######"
+cd ../advanced_async_time_request
+helics run --path=./async_runner.json
+
+echo "\n##### Connector interface creation ######"
+cd ../advanced_connector/interface_creation
+helics run --path=./connector_interface_creation_runner.json
+
+echo "\n##### Connector matchfile direct ######"
+cd ../matchfile
+# Currently not working
+# HELICS Examples issue #101
+# helics run --path=./connector_matchfile_direct_runner.json
+helics run --path=./connector_matchfile_regex_runner.json
+
+echo "\n##### Advanced default Pythonic ######"
+cd ../../advanced_default_pythonic/
+helics run --path=./advanced_default_runner.json
+
+# Graphing needs to be looked at
+# echo "\n##### Dyanmic federation ######"
+cd ../advanced_dynamic_federation/
+# helics run --path=./dynamic_federation_runner.json
+
+echo "\n##### Single core federation ######"
+cd ../advanced_single_core
+helics run --path=./single_core_runner.json
