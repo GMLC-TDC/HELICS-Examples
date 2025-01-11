0781cd7a441fa7ba97c2454ca5e91f8e4506301b
trevorhardy <trevor.hardy@pnnl.gov>
2025-01-10 17:14:05 -0800

    Workflow debugging

--- user_guide_examples/run_advanced_user_guide_examples.sh
+++ user_guide_examples/run_advanced_user_guide_examples.sh
@@ -16,12 +16,12 @@ fi
 
 echo "\n###### Advanced broker hierarchies #####"
 cd ./advanced/advanced_brokers/hierarchies
-helics run --path=./broker_hierarchy_runner_A.json &
-helics run --path=./broker_hierarchy_runner_B.json &
- helics run --path=./broker_hierarchy_runner_C.json
+# helics run --path=./broker_hierarchy_runner_A.json &
+# helics run --path=./broker_hierarchy_runner_B.json &
+# helics run --path=./broker_hierarchy_runner_C.json
 
 # Fails due to broker port out of range
-echo "\n###### Advanced multi-broker -    SKIPPING    ######"
+echo "\n###### Advanced multi-broker     SKIPPING    ######"
 echo "\tHELICS Issue #2721"
 echo "\n"
 cd ../multi_broker
@@ -29,63 +29,60 @@ cd ../multi_broker
 
 
 echo "\n###### Advanced simultaneous co-simulations ######"
-cd ../simultaneous/federation_1
+# cd ../simultaneous/federation_1
 helics run --path=./federation_1_runner.json &
-cd ../federation_2
+# cd ../federation_2
 helics run --path=./federation_2_runner.json &
-cd ../federation_3
+# cd ../federation_3
 helics run --path=./federation_3_runner.json
 
-echo "\n##### Advanced input and output - SKIPPING ######"
+echo "\n##### Advanced input and output     SKIPPING      ######"
 echo "\tHELICS issue #2542"
-echo "\n"
 cd ../../../advanced_input_output
 # helics run --path=./fib_runner.json
 
 echo "\n###### Iteration ######"
 cd ../advanced_iteration
-helics run --path=./advanced_iteration_runner.json
+# helics run --path=./advanced_iteration_runner.json
 
 echo "\n##### Multi-input #####"
 cd ../advanced_message_comm/multi_input
-helics run --path=./multi_input_runner.json
+# helics run --path=./multi_input_runner.json
 
 echo "\n###### Queries ######"
 cd ../query
-helics run --path=./query_runner.json
+# helics run --path=./query_runner.json
 
-echo "\n##### Translators - SKIPPING ######"
+echo "\n##### Translators    SKIPPING      ######"
 echo "\tHELICS issue #2562"
-echo "\n"
 cd ../translators
 # helics run --path=./translator_runner.json
 
 echo "\n##### Advanced default ######"
 cd ../../advanced_default
-helics run --path=./advanced_default_runner.json
+# helics run --path=./advanced_default_runner.json
 
 echo "\n##### Async time request ######"
 cd ../advanced_async_time_request
-helics run --path=./async_runner.json
+# helics run --path=./async_runner.json
 
 echo "\n##### Connector interface creation ######"
 cd ../advanced_connector/interface_creation
-helics run --path=./connector_interface_creation_runner.json
+# helics run --path=./connector_interface_creation_runner.json
 
-echo "\n##### Connector matchfile direct - SKIPPING ######"
+echo "\n##### Connector matchfile direct    SKIPPING     ######"
 echo "\tHELICS-Example issue #101"
-echo "\n"
 cd ../matchfile
 # helics run --path=./connector_matchfile_direct_runner.json
 
 echo "\n##### Connector matchfile regex ######"
-helics run --path=./connector_matchfile_regex_runner.json
+# helics run --path=./connector_matchfile_regex_runner.json
 
 echo "\n##### Advanced default Pythonic ######"
 cd ../../advanced_default_pythonic/
-helics run --path=./advanced_default_runner.json
+# helics run --path=./advanced_default_runner.json
 
-echo "\n##### Dynamic federation  ######"
+echo "\n##### Dynamic federation ######"
 cd ../advanced_dynamic_federation/
 helics run --path=./dynamic_federation_runner.json
 
